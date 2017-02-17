36b1ba35e851331fd0c96eceadd63c5f38c7da9d
Uros Tomovic <urosh@Uross-MacBook-Pro.local>
2017-02-17 17:58:13 +0100

    orderinit

--- source/js/views/Order/index.jsx
+++ source/js/views/Order/index.jsx
@@ -1,5 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import SideDate from 'components/Client/SideDate';
+import OrderFood from 'components/Client/OrderFood';
+import OrderNote from 'components/Client/OrderNote';
+import OrderSummary from 'components/Client/OrderSummary';
+// import moment from 'moment';
 
 @connect(state => ({
   loggedInUser: state.login.get('loggedInUser'),
@@ -8,13 +13,31 @@ export default class Order extends Component {
   static propTypes = {
     loggedInUser: PropTypes.object,
   }
+  // render() {
+  //   const { loggedInUser } = this.props;
+  //   return (
+  //     <div className='Order'>
+  //       <h1>Hello, { loggedInUser && loggedInUser.username }</h1>
+  //     </div>
+  //   );
+  // }
 
   render() {
-    const { loggedInUser } = this.props;
     return (
       <div className='Order'>
-        <h1>Hello, { loggedInUser && loggedInUser.username }</h1>
+        <div className='MyOrder-wrapper'>
+          <SideDate />
+        </div>
       </div>
     );
+    // {this.state.foodArray.map((item, index) => (
+    //   <OrderFood
+    //     key={ index }
+    //     food={ item }
+    //     text={ this.state.foodText[index] }
+    //   />
+    // ))}
+    // <OrderNote />
+    // <OrderSummary />
   }
 }
